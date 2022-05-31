8730e30eec67af7be5a28a204c80d92358555a0e
Karolina Sulik <karolinaworoniak98@gmail.com>
2022-05-31 13:25:36 +0200

    Short documentation for the decode function added

--- Laender-App/Extensions/Bundle+Ext.swift
+++ Laender-App/Extensions/Bundle+Ext.swift
@@ -8,7 +8,17 @@
 import Foundation
 
 extension Bundle {
-    func decode<T: Decodable>(fileName: String) -> T {
+    /// Diese Methode kann für das Laden einer JSON-Datei aus dem lokalen Bundle verwendet werden.
+    ///
+    /// Description
+    /// - Parameter fileName: Der Dateiname der JSON-Datei, die dekodiert werden soll. **Wichtig**: Die Endung .json muss mit angegeben werden<##>
+    ///
+    ///
+    /// ```
+    /// let countries: [Country] = Bundle.main.decode("countries.json")//Beispiel
+    /// ```
+    /// - Returns: Gibt die dekodierten JSON-Daten in dem angegebenen Swift-Datentyp zurück.
+    func decode<T: Decodable>(fileName: String, a: Int? = nil) -> T {
         guard let url = self.url(forResource: fileName, withExtension: nil) else {
             fatalError("Datei konnte im Bundle nicht gefunden werden.")
         }
@@ -20,6 +30,7 @@ extension Bundle {
         guard let objects = try? JSONDecoder().decode(T.self, from: data) else {
             fatalError("Dekodierung ist schiefgelaufen")
         }
+        
         
         return objects
     }
